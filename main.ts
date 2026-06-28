8c38773e13945462965d904c190dcf0b0c69b6cd
Jacob Torrång <kopatheonlyone@hotmail.com>
2026-06-28 14:47:56 +0200

    Update main

--- main.ts
+++ main.ts
@@ -2,14 +2,6 @@ import { IC } from "./src/ic.ts";
 import { Expr } from "./src/expr.ts";
 import { Mod } from "./src/mod.ts";
 
-type Format<self> = {
-  fmt: (value: self) => string;
-};
-
-type Emit<from, to> = {
-  emit: (value: from) => to;
-};
-
 const program: IC = {
   tag: "prim",
   prim: "add",
@@ -19,18 +11,19 @@ const program: IC = {
   ],
 };
 
-IC satisfies Format<IC> & Emit<IC, Expr>;
 const expr = IC.emit(program);
 
-Expr satisfies Format<Expr> & Emit<Expr, string>;
 const mod: Mod = {
   funcs: {
-    main: { result: Expr.type(expr), body: Expr.emit(expr) },
+    main: {
+      name: "main",
+      result: Expr.type(expr),
+      body: Expr.emit(expr),
+    },
   },
   exports: ["main"],
 };
 
-Mod satisfies Emit<Mod, string>;
 const watText = Mod.emit(mod);
 
 await Deno.mkdir("build", { recursive: true });
@@ -39,8 +32,8 @@ await Deno.writeTextFile("build/out.wat", watText);
 console.log("IC:");
 console.log(IC.fmt(program));
 
-console.log("\nExpr:");
+console.log("Expr:");
 console.log(Expr.fmt(expr));
 
-console.log("\nWAT:");
+console.log("WAT:");
 console.log(watText);
